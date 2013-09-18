bcce8a39ce4d6f777763161b6b24a09958f3c9d6
Karol Flis <unn@o2.pl>
2013-09-18 20:43:24 +0200

    Update#1

---
+++ dbIndex.php
@@ -0,0 +1,24 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', true);
+
+$dsn = 'mysql:dbname=dev_cms;host=127.0.0.1';
+$user = 'root';
+$password = '1234';
+
+try
+{
+	$dbh = new PDO($dsn, $user, $password);
+	$query = $dbh->prepare('SELECT * FROM workers WHERE  user_id>? AND user_id<?');
+	$query->execute(array(0,2));
+
+	while ($var = $query->fetch(PDO::FETCH_ASSOC))
+	{
+		echo '<pre>';
+		var_dump($var);
+	}
+}
+catch(PDOException $e)
+{
+	echo 'Connection failed'. $e->getMessage();
+}
