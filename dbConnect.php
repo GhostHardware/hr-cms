61f6e2a34ed016a4ab58b270aa151a3badae37ef
GhostHardware <de.kosinski@gmail.com>
2013-09-19 23:57:26 +0200

    added function to send queries to database also started working on database connection

---
+++ dbConnect.php
@@ -0,0 +1,4 @@
+<?php
+$databaseName = 'mysql:dbname=dev_cms;host=127.0.0.1'; // dummy values, switch with real ones
+$databaseUser = 'root';
+$databasePassword = '1234';
--- dbIndex.php
+++ dbIndex.php
@@ -6,19 +6,24 @@ $dsn = 'mysql:dbname=dev_cms;host=127.0.0.1';
 $user = 'root';
 $password = '1234';
 
-try
+function getDBInfo($dsn, $user, $password) // to do: make it work on other sql queries
 {
-	$dbh = new PDO($dsn, $user, $password);
-	$query = $dbh->prepare('SELECT * FROM workers WHERE  user_id>? AND user_id<?');
-	$query->execute(array(0,2));
-
-	while ($var = $query->fetch(PDO::FETCH_ASSOC))
+	try
 	{
-		echo '<pre>';
-		var_dump($var);
+		$dbh = new PDO($dsn, $user, $password);
+		$query = $dbh->prepare('SELECT * FROM workers WHERE  user_id>? AND user_id<?');
+		$query->execute(array(0,2));
+
+		while ($var = $query->fetch(PDO::FETCH_ASSOC))
+		{
+			echo '<pre>';
+			var_dump($var);
+		}
+	}
+	catch(PDOException $e)
+	{
+		echo 'Connection failed'. $e->getMessage();
 	}
 }
-catch(PDOException $e)
-{
-	echo 'Connection failed'. $e->getMessage();
-}
+
+getDBInfo($dsn, $user, $password);
